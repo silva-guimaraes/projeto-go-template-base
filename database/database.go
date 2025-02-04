5a45c2329a7c9cf7d601df486532d1aa1a06481c
silva guimaraes <silva__guimaraes@hotmail.com>
2025-02-04 17:28:33 -0300

    Aplicar migrações ao rodar servidor

--- database/database.go
+++ database/database.go
@@ -3,15 +3,20 @@ package database
 import (
 	"context"
 	"database/sql"
+	"embed"
 	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/joho/godotenv"
+	"github.com/pressly/goose/v3"
 )
 
 var instance *sql.DB
+
+//go:embed migrations/*.sql
+var embedMigrations embed.FS
 
 func New() *sql.DB {
 
@@ -25,9 +30,10 @@ func New() *sql.DB {
 	if err != nil {
 		panic(err)
 	}
-	// Para rodar os testes desse pacote, É necessário que haja um symlink de um .env local
-	// apontando para o .env do topo do projeto, já que 'go test' usa o diretório raiz do
-	// pacote (este) para executar os testes enquanto 'go run' usa o diretório raiz do projeto.
+	// Para rodar os testes desse pacote, É necessário que haja um symlink de um .env nesta
+	// pasta (database/) apontando para o .env no topo do projeto, já que 'go test' usa o
+	// diretório raiz do pacote (./database) para executar os testes enquanto 'go run' usa o diretório
+	// raiz do projeto.
 
 	// variáveis ambientais de .env são carregadas para o ambiente do nosso processo, basta
 	// usar a forma padrão para pegar os valores dessas variáveis.
@@ -47,6 +53,15 @@ func New() *sql.DB {
 
 	// Verifica se a conexão foi um sucesso
 	if err := db.Ping(); err != nil {
+		panic(err)
+	}
+
+	goose.SetBaseFS(embedMigrations)
+	if err := goose.SetDialect("postgres"); err != nil {
+		panic(err)
+	}
+	// Aplica todas as migrações disponíveis
+	if err := goose.Up(db, "migrations"); err != nil {
 		panic(err)
 	}
 
